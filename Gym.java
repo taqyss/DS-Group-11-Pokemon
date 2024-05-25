4d4211413a9e8667d7f3753427614e96bb5be4ec
REVOL07 <153249804+REVOL07@users.noreply.github.com>
2024-05-25 16:13:45 +0800

    Update Gym.java

--- Gym.java
+++ Gym.java
@@ -2,35 +2,120 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ds_project;
+package project.pikachu; //3 weeks commit
 
-/**
- *
- * @author Taqy
- */
 import java.util.ArrayList;
 import java.util.List;
 
-public class Gym {
-    private String leaderName;
-    private List<Pokemon> leaderPokemon;
 
-    public Gym(String leaderName) {
+public class Gym {
+    
+    private String leaderName=" ";
+    private List<Pokemon> leaderPokemon;
+    private int leaderlevel;
+    private List<String> leaderStrongAgainst;
+    private List<String> leaderWeakAgainst;
+    private String gymBadges;
+    private int leaderHitPoints;
+    private int leadercurrentHitPoints;
+    private int leaderXP;
+    private int leadercurrentXP;
+    
+
+    public Gym(String leaderName , List<Pokemon> leaderPokemon , int leaderlevel , List<String> leaderStrongAgainst , List<String>  leaderWeakAgainst , String gymBadges , int leaderHitPoints , int leaderXP , int leadercurrentHitPoints , int leadercurrentXP ) {
         this.leaderName = leaderName;
         this.leaderPokemon = new ArrayList<>();
+        this.leaderlevel=leaderlevel;
+        this.leaderStrongAgainst=new ArrayList<>();
+        this.leaderWeakAgainst=new ArrayList<>();
+        this.gymBadges=gymBadges;
+        this.leaderXP=leaderXP;
+        this.leadercurrentXP=leadercurrentXP;
+        this.leaderHitPoints=leaderHitPoints;
+        this.leadercurrentHitPoints=leadercurrentHitPoints;
     }
 
     public void addPokemon(Pokemon pokemon) {
         leaderPokemon.add(pokemon);
     }
 
-    public String getLeaderName() {
+    public String getleaderName() {
         return leaderName;
     }
+    
+    public void setleaderName(String leaderName) {
+        this.leaderName=leaderName;
+    }
+    
+     public String getgymBadges() {
+        return gymBadges;
+    }
+    
+    public void setgymBadges(String gymBadges) {
+        this.gymBadges=gymBadges;
+    }
+    
 
-    public List<Pokemon> getLeaderPokemon() {
+    public List<Pokemon> getleaderPokemon() {
         return new ArrayList<>(leaderPokemon);
     }
-}
+    
+     public void setleaderPokemon(Pokemon pokemon) {
+     this.leaderPokemon.add(pokemon);
+    }
+    
+    public int getleaderLevel(){
+        return leaderlevel;
+    }
+    
+      public List<String>  getleaderStrongAgainst (){
+        return new ArrayList<>(leaderStrongAgainst);
+    }
+    
+    public void setleaderStrongAgainst(String StrongAgainst){
+        this.leaderStrongAgainst =new ArrayList<>();   
+    }
+    
+    public List<String> getleaderWeakAgainst (){
+        return new ArrayList<>(leaderWeakAgainst);
+    }
+    
+     public void setleaderWeakAgainst (String WeakAgainst ){
+        this.leaderWeakAgainst = new ArrayList<>();
+     }    
+     
+        public int getleaderHitPoints(){
+        return leaderHitPoints;
+    }
 
+     public void setleaderHitPoints(int hp) {
+        this.leaderHitPoints = leaderHitPoints;
+    }
+    
+     public int getleadercurrentHitPoints() {
+        return leadercurrentHitPoints;
+    }
+     
+     public int getleaderXP (){
+        return leaderXP;
+    }
+    
+     public void setleaderXP ( int leaderXP ){
+        this.leaderXP = leaderXP;
+    }
+    
+    public int getleadercurrentXP (){
+        return leadercurrentXP;
+    }
+    
+    public void setleadercurrentXP ( int leadercurrentXP ){  
+        this.leadercurrentXP = leadercurrentXP;
+    }
+       
+
+    public void setleadercurrentHitPoints(int leadercurrenthitPoints) {
+        this.leadercurrentHitPoints = leadercurrenthitPoints;
+    }
+          
+}
 
