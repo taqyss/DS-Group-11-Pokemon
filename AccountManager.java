78ba3626165581a73a990ef328451d4e0b456099
nadyacamila <155145914+nadyacamila@users.noreply.github.com>
2024-06-08 14:58:43 +0700

    AccountManager.java

--- AccountManager.java
+++ AccountManager.java
@@ -8,7 +8,6 @@ import java.util.*;
 
 public class AccountManager {
     private Map<String, Account> accounts; //stores username --> password
-    private Map<String, Player> playerAcc; // store username --> player object
     private Player currentPlayer;
     private final SaveManager saveManager;
     Scanner sc = new Scanner(System.in);
@@ -18,7 +17,6 @@ public class AccountManager {
         if(this.accounts==null){
             this.accounts = new HashMap<>();
         }
-        this.playerAcc = new HashMap<>();
         this.saveManager = saveManager;
     }
     
@@ -32,11 +30,11 @@ public class AccountManager {
             System.out.println("Account already exists");
         }
         else{
-            Account newAcc = new Account(username, password, username);
-            Player newPlayer = new Player(username, "Pallet Town", new ArrayList<>(), new ArrayList<>());
-            newAcc.setPlayer(newPlayer);
-            saveManager.addAccount(newAcc);
-            System.out.println("Account creation successfull");
+        Player player = new Player(username, "Pallet Town", new ArrayList<>(), new ArrayList<>());
+        Account account = new Account(username, password, player);
+        saveManager.addAccount(account);
+        currentPlayer = player;
+        System.out.println("Account creation successful");
         }
     }
     
@@ -51,7 +49,7 @@ public class AccountManager {
         for (Account account : accounts.values()) {
             if (account.getUsername().equals(username) && account.getPassword().equals(password)) {
                 System.out.println("Login Successful");
-                currentPlayer = account.getPlayer();
+                currentPlayer = saveManager.loadGame(username);
                 found = true;
                 break;
             }
