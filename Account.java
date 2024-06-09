57d26b697e9adcc39e7c0867d5785adfc6e35b89
nadyacamila <155145914+nadyacamila@users.noreply.github.com>
2024-06-09 20:25:13 +0700

    Account.java

--- Account.java
+++ Account.java
@@ -10,30 +10,32 @@ public class Account implements Serializable{
     private Player player;
     private String playerName;
     
+    //constructor for new account object
     public Account(String username, String password, Player player){
         this.username = username;
         this.password = password;
         this.playerName = playerName;
-        this.player = player;
+        this.player = player; //connect player object with this account
     }
     
+    //getter and setter
     public String getPlayerName(){
         return playerName;
     }
 
-    public String getUsername() {
+    public String getUsername(){
         return username;
     }
 
-    public String getPassword() {
+    public String getPassword(){
         return password;
     }
 
-    public Player getPlayer() {
+    public Player getPlayer(){
         return player;
     }
 
-    public void setPlayer(Player player) {
+    public void setPlayer(Player player){
         this.player = player;
     }
 }
